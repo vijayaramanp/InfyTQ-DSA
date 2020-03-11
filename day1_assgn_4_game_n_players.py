99ad9fbcb7d0b2cd0caa1ead936ee8f2c5ef0dc5
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-11 11:23:10 +0530

    Create day1_assgn_4_game_n_players.py

---
+++ day1_assgn_4_game_n_players.py
@@ -0,0 +1,53 @@
+#DSA-Assgn-4
+
+class Player:
+    def __init__(self,name,experience):
+        self.__name=name
+        self.__experience=experience
+
+    def get_name(self):
+        return self.__name
+
+    def get_experience(self):
+        return self.__experience
+
+    def __str__(self):
+        return(self.__name+" "+(str)(self.__experience))
+
+class Game:
+    def __init__(self,players_list):
+        self.__players_list=players_list
+        
+    def sort_players_based_on_experience(self):
+        self.__players_list.sort(key=lambda x:x.get_experience(), reverse=True)   
+    def shift_player_to_new_position(self, old_index_position, new_index_position):
+        temp=self.__players_list[old_index_position]
+        self.__players_list[old_index_position]=self.__players_list[new_index_position]
+        self.__players_list[new_index_position]=temp
+    
+    def display_player_details(self):
+        for player in self.__players_list:
+            print(player)
+
+player1=Player("Dhoni",15)
+player2=Player("Virat",10)
+player3=Player("Rohit",12)
+player4=Player("Raina",11)
+player5=Player("Jadeja",13)
+player6=Player("Ishant",9)
+player7=Player("Shikhar",8)
+player8=Player("Axar",7.5)
+player9=Player("Ashwin",6)
+player10=Player("Stuart",7)
+player11=Player("Bhuvneshwar",5)
+#Add different values to the list and test the program
+players_list=[player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11]
+g=Game(players_list)
+g.sort_players_based_on_experience()
+print("Before Shift")
+g.display_player_details()
+g.shift_player_to_new_position(0,3)
+print("After Shift")
+g.display_player_details()
+
+#Create object of Game class, invoke the methods and test your program
