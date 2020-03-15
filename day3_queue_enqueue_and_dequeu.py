17b2e210e2aaa41b25b7174e046c2968a53721ed
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-15 07:24:30 +0530

    Create day3_queue_enqueue_and_dequeu.py

---
+++ day3_queue_enqueue_and_dequeu.py
@@ -0,0 +1,57 @@
+class Queue:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__rear=-1
+        self.__front=0
+
+    def is_full(self):
+        if(self.__rear==self.__max_size-1):
+                return True
+        return False
+
+    def is_empty(self):
+        if(self.__front>self.__rear):
+            return True
+        return False
+
+    def enqueue(self,data):
+        if(self.is_full()):
+            print("Queue is full!!!")
+        else:
+            self.__rear+=1
+            self.__elements[self.__rear]=data
+
+    def dequeue(self):
+        if(self.is_empty()):
+            print("Queue is empty!!!")
+        else:
+            data=self.__elements[self.__front]
+            self.__front+=1
+            return data
+
+    def display(self):
+        for index in range(self.__front, self.__rear+1):
+            print(self.__elements[index])
+
+    def get_max_size(self):
+        return self.__max_size
+    def __str__(self):
+        msg=[]
+        index=self.__front
+        while(index<=self.__rear):
+            msg.append((str)(self.__elements[index]))
+            index+=1
+        msg=" ".join(msg)
+        msg="Queue data(Front to Rear): "+msg
+        return msg
+
+queue1=Queue(5)
+queue1.enqueue(10)
+queue1.enqueue(100)
+queue1.enqueue(1000)
+
+data=queue1.dequeue()
+#Display all the elements in the queue.
+print("After dequeue")
+queue1.display()
