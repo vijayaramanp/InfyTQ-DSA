afaf6bd7196915545a5b4a94bf04f632d798b45e
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-15 07:11:01 +0530

    Create day3_pop_operation.py

---
+++ day3_pop_operation.py
@@ -0,0 +1,64 @@
+class Stack:
+    def __init__(self,max_size):
+        self.__max_size=max_size
+        self.__elements=[None]*self.__max_size
+        self.__top=-1
+
+    def is_full(self):
+        if(self.__top==self.__max_size-1):
+            return True
+        return False
+
+    def is_empty(self):
+        if(self.__top==-1):
+            return True
+        return False
+
+    def push(self,data):
+        if(self.is_full()):
+            print("The stack is full!!")
+        else:
+            self.__top+=1
+            self.__elements[self.__top]=data
+
+    def pop(self):
+        if(self.is_empty()):
+            print("The stack is empty!!")
+        else:
+            data= self.__elements[self.__top]
+            self.__top-=1
+            return data
+
+    def display(self):
+        if(self.is_empty()):
+            print("The stack is empty")
+        else:
+            index=self.__top
+            while(index>=0):
+                print(self.__elements[index])
+                index-=1
+
+    def get_max_size(self):
+        return self.__max_size
+
+    def __str__(self):
+        msg=[]
+        index=self.__top
+        while(index>=0):
+            msg.append((str)(self.__elements[index]))
+            index-=1
+        msg=" ".join(msg)
+        msg="Stack data(Top to Bottom): "+msg
+        return msg
+
+stack1=Stack(5)
+#Push all the required element(s).
+stack1.push("A")
+stack1.push("B")
+stack1.push("C")
+stack1.push("D")
+#Pop an element
+print(stack1.pop())
+print("After pop")
+stack1.display()
+
